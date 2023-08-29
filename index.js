b8493187d707e08c4a6c41898e953124f73d9aef
Paulo Salvatore <salvatoregames@gmail.com>
2023-08-29 10:06:35 -0300

    Endpoint Read All implementado

--- index.js
+++ index.js
@@ -11,4 +11,13 @@ app.get("/oi", function (req, res) {
   res.send("Olá, mundo!");
 });
 
+// Endpoints de Herois
+const lista = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+//             0                    1                2
+
+// Read All -> [GET] /herois
+app.get("/herois", function (req, res) {
+  res.send(lista);
+});
+
 app.listen(3000);
