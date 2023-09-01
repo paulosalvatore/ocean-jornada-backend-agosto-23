8057ceebb8a31979fb20f8e834c6bf56384b8386
Elton De Santana <134665630+eltonanderson@users.noreply.github.com>
2023-09-01 11:40:20 -0300

    atualizado url mongoDB

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
 // const url = "mongodb://localhost:27017";
-const url = "mongodb://127.0.0.1:27017";
-// const url = "mongodb+srv://admin:V90K7ehx2krw7OlM@cluster0.gbnr4oi.mongodb.net";
+//const url = "mongodb://127.0.0.1:27017";
+const url = "mongodb+srv://admin:xygUJlVOzVYZ9zjd@cluster0.0uasqgu.mongodb.net";
 const dbName = "jornada-backend-agosto-23";
 const client = new MongoClient(url);
 
